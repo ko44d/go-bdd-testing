b7d632832b4a33b1fd92fdb71cea092c6b5dbffb
koyoshid <koyoshid622@gmail.com>
2021-06-02 00:58:48 +0900

    テストパターンと仕様の追加

--- book.go
+++ book.go
@@ -1,5 +1,7 @@
 package go_bdd_testing
 
+import "encoding/json"
+
 type book struct {
 	Title  string
 	Author string
@@ -8,19 +10,42 @@ type book struct {
 
 type Book interface {
 	CategoryByLength() string
+	GetTitle() string
+	GetAuthor() string
+	GetPages() int64
 }
 
 func NewBook(title, author string, pages int64) Book {
 	return &book{
-		Title: title,
+		Title:  title,
 		Author: author,
-		Pages: pages,
+		Pages:  pages,
 	}
 }
 
-func(b *book) CategoryByLength() string {
+func NewBookFromJSON(object string) (*book, error) {
+	var book book
+	if err := json.Unmarshal([]byte(object), &book); err != nil {
+		return &book, err
+	}
+	return &book, nil
+}
+
+func (b *book) CategoryByLength() string {
 	if b.Pages >= 300 {
 		return "Novel"
 	}
 	return "Short story"
+}
+
+func (b *book) GetTitle() string {
+	return b.Title
+}
+
+func (b *book) GetAuthor() string {
+	return b.Author
+}
+
+func (b *book) GetPages() int64 {
+	return b.Pages
 }
--- book_test.go
+++ book_test.go
@@ -6,18 +6,18 @@ import (
 )
 
 var _ = Describe("Book", func() {
-	var (
-		longBook Book
-		shortBook Book
-	)
-
-	BeforeEach(func() {
-		longBook = NewBook("title1", "author1", 1000)
-		shortBook = NewBook("title2", "author2", 10)
-	},
-	)
 
 	Describe("Categorizing book length", func() {
+		var (
+			longBook  Book
+			shortBook Book
+		)
+
+		BeforeEach(func() {
+			longBook = NewBook("title1", "author1", 1000)
+			shortBook = NewBook("title2", "author2", 10)
+		},
+		)
 		Context("With more than 300 pages", func() {
 			It("should be a novel", func() {
 				actual := longBook.CategoryByLength()
@@ -32,4 +32,29 @@ var _ = Describe("Book", func() {
 		})
 	},
 	)
+
+	Describe("loading from JSON", func() {
+		var (
+			book Book
+			err  error
+		)
+		BeforeEach(func() {
+			book, err = NewBookFromJSON(`{
+            "title":"Les Miserables",
+            "author":"Victor Hugo",
+            "pages":2783
+			}`)
+		})
+
+		Context("when the JSON parses successfully", func() {
+			It("should populate the fields correctly", func() {
+				Expect(book.GetTitle()).To(Equal("Les Miserables"))
+				Expect(book.GetAuthor()).To(Equal("Victor Hugo"))
+				Expect(book.GetPages()).To(Equal(int64(2783)))
+			})
+			It("should not error", func() {
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+	})
 })

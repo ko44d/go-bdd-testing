dfd54cee8caa8e3dafaaa3d243f4666c72751d7a
koyoshid <koyoshid622@gmail.com>
2022-02-09 00:09:10 +0900

    リファクタリング

--- howto-gomega-gexec/main_test.go
+++ howto-gomega-gexec/main_test.go
@@ -1,6 +1,7 @@
 package main_test
 
 import (
+	"fmt"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/onsi/gomega/gexec"
@@ -9,13 +10,15 @@ import (
 
 var _ = Describe("Main", func() {
 	var (
-		cli string
-		err error
+		buildPath string
+		cliPath   string
+		err       error
 	)
 
 	BeforeEach(func() {
-		cli, err = gexec.Build(".")
+		buildPath, err = gexec.Build(".")
 		Expect(err).ShouldNot(HaveOccurred())
+		cliPath = fmt.Sprintf("%s/%s", buildPath, "howto-gomega-gexec")
 	})
 
 	AfterEach(func() {
@@ -23,7 +26,7 @@ var _ = Describe("Main", func() {
 	})
 	Context("with hoge set in first option", func() {
 		It("return hoge in terminal", func() {
-			command := exec.Command(cli+"/howto-gomega-gexec", "-first hhhhh")
+			command := exec.Command(cliPath, "-first hhhhh")
 			session, err := gexec.Start(command, GinkgoWriter, GinkgoWriter)
 			session.Wait()
 			Expect(err).ShouldNot(HaveOccurred())
